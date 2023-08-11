fa5031d9b0605cbcc67e5d65693e48b8170dfdba
BuuHQ <BuuHQ@tpb.vn>
2023-08-11 13:56:25 +0700

    cm 4

--- src/main/java/sample/controller/HelloController.java
+++ src/main/java/sample/controller/HelloController.java
@@ -1,5 +1,6 @@
 package sample.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*", maxAge = 4800)
 @RestController
 @RequestMapping("/api/v1/")
+@Slf4j
 public class HelloController {
     @GetMapping("/hello")
     public String hello() {
+        log.info("test log hello");
         return "Hello World";
     }
 }
